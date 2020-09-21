19febb257a392b4dce86ea0973b88b7b03279c83
vaibhavpise7030 <43112557+vaibhavpise7030@users.noreply.github.com>
2020-09-21 12:47:25 +0530

    Add files via upload

---
+++ C_Logic/Day_8/Assignment8_2/Register.java
@@ -0,0 +1,70 @@
+package mypack;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="reg1")
+public class Register
+{
+	private int id;
+	private String name;
+	private String city;
+	private String email;
+	private String uname;
+	private String  password;
+	
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	@Override
+	public String toString() {
+		return "Register [id=" + id + ", name=" + name + ", city=" + city + ", email=" + email + ", uname=" + uname
+				+ ", password=" + password + "]";
+	}
+	
+	
+	
+	
+
+}
